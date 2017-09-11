085ed120c70d4ce096316eb01f636b98cd2a0354
Levi777L <levi777l@yahoo.com>
2017-09-11 14:57:14 -0600

    Create MRW.cs

---
+++ NewSaving/MRW.cs
@@ -0,0 +1,97 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MRW : Monobehaviour {
+
+  [HideInInspector]
+  public MixedRealityWorld = new MixedRealityWorld();
+    
+}
+
+[Serializable]
+public class MixedRealityWorld {
+
+  [SerializeField]
+  public string name;
+
+  [SerializeField]
+  public List<Diorama> dioramas = new List<Diorama>();
+  
+}
+
+[Serializable]
+public class Diorama {
+  
+  [SerializeField]
+  public List<MRWObject> objects = new List<MRWObject>();
+  
+  [SerializeField]
+  public List<ViewPoint> viewpoints = new List<ViewPoint>();
+  
+}
+
+[Serializable]
+public class ViewPoint {
+
+  [SerializeField]
+  public SerializableVector3 relativePos;
+  
+  [SerializeField]
+  public SerializableQuaternion relativeRot;
+  
+  [SerializeField]
+  public SerializableVector3 relativeScale;
+
+}
+
+[Serializable]
+public class MRWObject {
+
+  [SerializeField]
+  public int todID = -1;
+  
+  [SerializeField]
+  pulbic int uniqueID = -1;
+  
+  [SerializeField]
+  public SerializableVector3 savedLocalPos;
+  
+  [SerializeField]
+  public SerializableQuaternion savedLocalRot;
+  
+  [SerializeField]
+  public SerializableVector3 savedLocalScale;
+  
+  [SerializeField]
+  public int layer = -1;
+
+  [SerializeField]
+  public List<object> components = new List<object>();
+
+}
+
+[Serializable]
+public class MRWPose {
+
+  [SerializeField]
+  public List<SerializableQuaternion> transformRotations = new List<SerializableQuaternion>();
+  
+}
+
+[Serializable]
+public class MRWChat {
+
+  [SerializeField]
+  public string chatText = "";
+  
+  [SerializeField]
+  public float colorShift;
+  
+}
+
+
+[Serializable]
+public class MRWEvent {
+
+}
